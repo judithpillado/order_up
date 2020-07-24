5972bf52930f57d51e4da6664d23cb2fd144a82e
Judith Pillado <judith.pillado@gmail.com>
2020-07-24 12:00:45 -0500

    Begin Extension

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -8,11 +8,13 @@ RSpec.describe "Chef Show Page" do
     @ingredient2 = @dish1.ingredients.create!(name: "Guanciale", calories: 165)
     @ingredient3 = @dish1.ingredients.create!(name: "Egg Yolks", calories: 55)
     @ingredient4 = @dish1.ingredients.create!(name: "Pecorino Romano", calories: 119)
+    @ingredient5 = @dish1.ingredients.create!(name: "Spinach", calories: 119)
+    @ingredient6 = @dish1.ingredients.create!(name: "Tomato", calories: 119)
     @dish2 = @chef.dishes.create!(name: "Creamy Tortellini", description: "Cheesy pasta with spinach and tomato.")
-    @ingredient5 = @dish2.ingredients.create!(name: "Tortellini", calories: 249)
-    @ingredient6 = @dish2.ingredients.create!(name: "Spinach", calories: 23)
-    @ingredient7 = @dish2.ingredients.create!(name: "Tomato", calories: 33)
-    @ingredient8 = @dish2.ingredients.create!(name: "Pecorino Romano", calories: 119)
+    @ingredient7 = @dish2.ingredients.create!(name: "Tortellini", calories: 249)
+    @ingredient8 = @dish2.ingredients.create!(name: "Spinach", calories: 23)
+    @ingredient9 = @dish2.ingredients.create!(name: "Tomato", calories: 33)
+    @ingredient10 = @dish2.ingredients.create!(name: "Pecorino Romano", calories: 119)
   end
 
   it "Displays chef's name, a link to view all of the ingredients the chef uses in their dishes" do
@@ -27,6 +29,12 @@ RSpec.describe "Chef Show Page" do
     expect(page).to have_content(@ingredient5.name)
     expect(page).to have_content(@ingredient6.name)
     expect(page).to have_content(@ingredient7.name)
-    expect(page).to have_content(@ingredient8.name)
+  end
+
+  it "Displays the three most popular ingredients that the chef uses in their dishes" do
+    visit "/chefs/#{@chef.id}"
+    expect(page).to have_content(@ingredient4.name)
+    expect(page).to have_content(@ingredient5.name)
+    expect(page).to have_content(@ingredient6.name)
   end
 end
